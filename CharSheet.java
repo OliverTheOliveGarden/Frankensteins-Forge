ac56941fc913b9cf8e9055c7b2f2bd9c795ca338
Oliver Jonaitis <oliverjonaitis09@gmail.com>
2024-03-07 09:57:13 -0500

    orginized and added skillmodifer calcuations

--- CharSheet.java
+++ CharSheet.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
+import utils.ANSI;
+import utils.DiceRoller;
+
 public class CharSheet {
-    static final DNDStuf DNDStuf = new DNDStuf();
     static final DiceRoller DiceRoller = new DiceRoller();
+    static final Scanner in = new Scanner(System.in);
 
     public static int level = 1;
 
@@ -10,10 +15,101 @@ public class CharSheet {
     public static int inl = 0;
     public static int wis = 0;
     public static int cha = 0;
+
+    public static int strModifer = 0;
+    public static int dexModifer = 0;
+    public static int conModifer = 0;
+    public static int inlModifer = 0;
+    public static int wisModifer = 0;
+    public static int chaModifer = 0;
     
     public static String charName;
 
+    public static String Class;
+    public static String subClass;
+
+    public static boolean inspration = false;
+
+    public static int Proficiancy = 0;
+
+
+
     CharSheet(){
-        
+        creatCharSheet();
     }
-} 
+
+    private static void creatCharSheet () {
+        charName = askString("What is your charcters name: ");
+
+        str = askInt("what is charters str: ");
+        dex = askInt("what is charters dex: ");
+        con = askInt("what is charters con: ");
+        inl = askInt("what is charters int: ");
+        wis = askInt("what is charters wis: ");
+        cha = askInt("what is charters cha: ");
+
+        strModifer = getSkillModifer(str);
+        dexModifer = getSkillModifer(dex);
+        conModifer = getSkillModifer(con);
+        inlModifer = getSkillModifer(inl);
+        wisModifer = getSkillModifer(wis);
+        chaModifer = getSkillModifer(cha);
+
+        System.out.println("Your strModifer is: " + strModifer);
+        System.out.println("Your dexModifer is: " + dexModifer);
+        System.out.println("Your conModifer is: " + conModifer);
+        System.out.println("Your inlModifer is: " + inlModifer);
+        System.out.println("Your wisModifer is: " + wisModifer);
+        System.out.println("Your chaModifer is: " + chaModifer);
+    }
+
+    private static int askInt(String queston) {
+        System.out.print(queston);
+        try {
+            return Integer.parseInt(in.next()); 
+        } catch (Exception e) {
+            System.out.println(ANSI.RED + ANSI.HIGH_INTENSITY + "ERROR: Ivaild Input" + ANSI.SANE);
+            return askInt(queston);
+        }
+    }
+
+    private static String askString(String queston) {
+        System.out.print(queston);
+        try {
+            return in.next();
+        } catch (Exception e) {
+            System.out.println(ANSI.RED + ANSI.HIGH_INTENSITY + "ERROR: Ivaild Input" + ANSI.SANE);
+            return askString(queston);
+        }
+    }
+
+    //#region skills
+    public int strRoll() {
+        return DiceRoller.D20() + strModifer;
+    }
+
+    public int dexRoll() {
+        return DiceRoller.D20() + dexModifer;
+    }
+
+    public int conRoll() {
+        return DiceRoller.D20() + conModifer;
+    }
+
+    public int intRoll() {
+        return DiceRoller.D20() + inlModifer;
+    }
+
+    public int wisRoll() {
+        return DiceRoller.D20() + wisModifer;
+    }
+
+    public int chaRoll() {
+        return DiceRoller.D20() + chaModifer;
+    }
+
+    private static int getSkillModifer (int skill) {
+        return (skill - 10)/2;
+    }
+    //#endregion
+}
--- dnd/DnDConstants.java
+++ dnd/DnDConstants.java
@@ -1,6 +1,5 @@
-import java.util.HashMap;
 
-public class DNDStuf {
+public class DnDConstants {
 
     public final String[] strSkills = {"Athletics","Svaing Throw"};
     public final String[] dexSkills = {"Acrobatics","Sleight of Hand","Stealth","Svaing Throw"};
@@ -10,7 +9,6 @@ public class DNDStuf {
     public final String[] chaSkills = {"Deception","Intimidation","Performance","Persuasion","Svaing Throw"};
 
     public final String[] classes = {"Artificer","Barbarian","Bard","Cleric","Druid","Fighter","Monk","Paladin","Ranger","Rogue","Sorcerer","Warlock","Wizard"};
-    public static HashMap<String, String[]> subClassesDic = new HashMap<String, String[]>(); 
     public static final String[][] subClasses = {{"Alchemist","Armorer","Artillerist","Battle Smith"},
     {"Path of the Ancestral Guardian","Path of the Battlerager","Path of the Beast","Path of the Berserker","Path of the Giant","Path of the Storm Herald","Path of the Totem Warrior","Path of Wild Magic","Path of the Zealot",},
     {"College of Creatio","College of Eloquence","College of Glamour","College of Lore","College of Spirits","College of Swords","College of Valor","College of Whispers"},
@@ -24,12 +22,9 @@ public class DNDStuf {
     {"Aberrant Mind","Clockwork Soul","Divine Soul","Draconic Bloodline","Lunar Sorcery","Shadow Magic","Storm Sorcery","Wild Magic"},
     {"The Archfey","The Celestial","The Fathomless","The Fiend","The Genie","The Great Old One","The Hexblade","The Undead","The Undying"},
     {"School of Abjuration","Bladesinging","School of Conjuration","School of Divination","School of Enchantment","School of Evocation","School of Illusion","School of Necromancy","Order of Scribes","School of Transmutation","War Magic"}};
-    
-    DNDStuf(){
-        for (int i = 0;i < subClasses.length; i++) {
-            subClassesDic.put(classes[i], subClasses[i]);
-        }
-    }
 
+    public static final String[] races = {"Aarakocra ","Aasimar ","Bullywug","Custom Lineage","Dragonborn","Dragonborn (Base)","Dragonborn (Chromatic) ","Dragonborn (Gem)","Dragonborn (Metallic) ","Dwarf","Dwarf (Hill)","Dwarf (Mountain)","Elf","Elf (Drow)","Elf (Eladrin)","Elf (High)","Elf (Wood)","Gnoll","Gnome","Gnome (Deep)","Gnome (Forest)","Gnome (Rock)","Goblin","Grimlock","Half-Elf","Half-Elf (Base)","Half-Orc","Half-Orc (Base)","Halfling","Halfling (Lightfoot)","Halfling (Stout)","Hobgoblin","Human","Human (Base)","Human (Variant)","Kenku","Kobold","Kuo-Toa","Lizardfolk","Merfolk","Orc","Skeleton","Tiefling","Tiefling (Base)","Troglodyte","Zombie"};
+    
+    public static final String[] Alinments = {"Chaotic Good", "Neutral Good", "Lawful Good", "Chaotic Neutral", "True Neutral", "Lawful Neutral", "Chaotic Evil", "Neutral Evil", "Lawful Neutral"};
 }
 
