5c93f05dda92e4f6115913673efb23dba8067bcb
Oliver Jonaitis <oliverjonaitis09@gmail.com>
2024-03-11 22:26:47 -0400

    fixed me being a silly gosse

--- src/CharSheet.java
+++ src/CharSheet.java
@@ -10,50 +10,50 @@ import dnd.DnDConstants;
 
 public class CharSheet { 
     //#region Golbal Vars
-    private static final DiceRoller DiceRoller = new DiceRoller();
-    private static final Scanner in = new Scanner(System.in);
+    private  final DiceRoller DiceRoller = new DiceRoller();
+    private  final Scanner in = new Scanner(System.in);
     
-    static int level = 1;
+     int level = 1;
 
-    static int str = 0;
-    static int dex = 0;
-    static int con = 0;
-    static int inl = 0;
-    static int wis = 0;
-    static int cha = 0;
+     int str = 0;
+     int dex = 0;
+     int con = 0;
+     int inl = 0;
+     int wis = 0;
+     int cha = 0;
  
-    static int strModifer = 0;
-    static int dexModifer = 0;
-    static int conModifer = 0;
-    static int inlModifer = 0;
-    static int wisModifer = 0;
-    static int chaModifer = 0;
+     int strModifer = 0;
+     int dexModifer = 0;
+     int conModifer = 0;
+     int inlModifer = 0;
+     int wisModifer = 0;
+     int chaModifer = 0;
  
-    static String charName;
+     String charName;
  
-    static String Class;
-    static String subClass;
+     String Class;
+     String subClass;
  
-    static boolean inspration = false;
-    static int proficiancy = 0;
+     boolean inspration = false;
+     int proficiancy = 0;
  
-    static int AC;
-    static int initiative;
-    static int speed;
+     int AC;
+     int initiative;
+     int speed;
  
-    static String hitDice;
-    static int numOfHitDice;
+     String hitDice;
+     int numOfHitDice;
  
-    static ArrayList<String> atksNames = new ArrayList<String>();
-    static ArrayList<Attack> Attacks = new ArrayList<Attack>();
-    static HashMap<String, Attack> atks = new HashMap<String, Attack>();
+     ArrayList<String> atksNames = new ArrayList<String>();
+     ArrayList<Attack> Attacks = new ArrayList<Attack>();
+     HashMap<String, Attack> atks = new HashMap<String, Attack>();
     //#endregion
     //#region Sheet Functions
     CharSheet(){
-        creatCharSheet();
+       
     }
 
-    private static void creatCharSheet () {
+    private void creatCharSheet () {
         charName = askString("What is your charcters name: ");
         
         Class = askChoices("Pick a Class", DnDConstants.classes);
@@ -76,12 +76,12 @@ public class CharSheet {
     }
     //#endregion
     //#region Print Function
-    private static String askChoices(String queston, String[] choices) {
+    private  String askChoices(String queston, String[] choices) {
         System.out.println(queston);
         for(int i = 0; i < choices.length - 1; i++) {
             System.out.print(i + 1 + ": " + choices[i] + ", ");
         }
-        System.out.println(choices.length + ": " + choices[choices.length]);
+        System.out.println(choices.length + ": " + choices[choices.length - 1]);
 
         System.out.println("");
 
@@ -93,7 +93,7 @@ public class CharSheet {
         }
     }
 
-    private static int askInt(String queston) {
+    private  int askInt(String queston) {
         System.out.print(queston);
         try {
             return Integer.parseInt(in.next()); 
@@ -103,7 +103,7 @@ public class CharSheet {
         }
     }
 
-    private static String askString(String queston) {
+    private  String askString(String queston) {
         System.out.print(queston);
         try {
             return in.next();
@@ -158,11 +158,11 @@ public class CharSheet {
         return DiceRoller.D20() + chaModifer;
     }
 
-    private static int getSkillModifer (int skill) {
+    private  int getSkillModifer (int skill) {
         return (skill - 10)/2;
     }
 
-    private static void updateSkillModifers () {
+    private  void updateSkillModifers () {
         strModifer = getSkillModifer(str);
         dexModifer = getSkillModifer(dex);
         conModifer = getSkillModifer(con);
